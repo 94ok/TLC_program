16dd10faf98bfa69dd3f8b2efbc73b75cd1e517c
94ok <94yhlee@snu.ac.kr>
2024-12-24 17:23:14 +0900

    241224-1723

--- py_functions/__init__.py
+++
@@ -1,5 +0,0 @@
-# One can use "from py_functions import 'fuction'" at main code to recall the function.
-# Before that one have to add the path with using "sys.path.append('D:/FILES/programming/Python/DataAnalyzingCodes/py_functions/')"
-# One have to add "from .'function' import 'function'" at below.
-
-from .im_rotate import im_rotate
